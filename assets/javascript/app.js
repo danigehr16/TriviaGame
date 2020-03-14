a8fa52e7e4dc836d40717113341317c2793e44b1
dani <gehr.danielle@gmail.com>
2020-03-14 00:42:40 -0400

    added a start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,71 @@
-// create a display for the Timer html
 
 console.log("hello")
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unansweredQuestions = 0;
+var timeLeft = 3;
+var intervalID;
+var indexQandA = 0;
+// var answered = fasle;
+var correct;
 
-var timer
+var VolleyballTrivaGame = [{
+    // questions will go here
+}]
+
+// function startGame() {
+//     console.log("the game has begun");
+//     $('.start-button').remove();
+//     correctAnswers = 0;
+//     incorrectAnswers = 0;
+//     unansweredQuestions = 0;
+//     loadQandA();
+// }
+
+function run() {
+    clearInterval(intervalId);
+    intervalID = setInterval(decrement, 3000);
+}
+
+function decrement() {
+    number--;
+    $("#timer").html("<h2>" + timeLeft + "</h2>");
+
+    if(number === 0) {
+        stop();
+        alert("TIME IS UP!!!")
+        function stop() {
+            clearInterval(intervalId)
+        }
+        run();
+        
+    }
+}
+
+
+
+
+$("timer").text("00:00");
 
 
 // update counter (count down) every 1 second
 
-var x = setInterval (function() {
-    var minutes = Math.floor((timer % ( 1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timer % (1000 *60)) / 1000);
-})
+
+
 
 // calculate for minuets and seconds
+
+// var deadline = new timer("0:03:00").getTime();
 
 // show the results as it counts down
 
 // document.getElementsById("count").innerHTML = minutes + "m" + seconds + "s";
 
 // when timer reaches 0 alert quiz taker there is no more time. also show the questions that were right and the questions that were wrong.
-if( distance < 0) {
-    clearInterval (x);
-    document.getElementById("count").innerHTML = "Times Up!!!";
-};
+// if( distance < 0) {
+//     clearInterval (x);
+//     document.getElementById("count").innerHTML = "Times Up!!!";
+// };
 
 // show a restart button
 
