e04873d0545cb67e6157ef1f9351398adcd18165
dani <gehr.danielle@gmail.com>
2020-03-15 14:36:25 -0400

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,96 @@
 
 console.log("hello")
+
+// these variables are for the questions
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
-var timeLeft = 3;
-var intervalID;
 var indexQandA = 0;
-// var answered = fasle;
-var correct;
+
+// these variables are for the timer
+
+var intervalID;
+
+// var minuets = 0
+// var seconds = 0
+var timeLeft = 10;
 
 var VolleyballTrivaGame = [{
     question: "What skill is used with two hands above the forehead and is normally used to set up a hitter to hit?",
-    answer:["A Pass" , "A Set" , "At Hit" , "A Dink"],
-    correct: "2",
+    answer: ["A Pass", "A Set", "At Hit", "A Dink"],
+    correct: "1",
     // image: (place image here!)
-    // questions will go here
+    // questions will go here.. put them in html to hard code them... 
 }]
 
 function startGame() {
     console.log("the game has begun");
-    $('.start-button').remove();
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    unansweredQuestions = 0;
-    loadQandA();
-}
+    $('.btn').remove();
+    $("#restartBtn").hide;
+    // correctAnswers = 0;
+    // incorrectAnswers = 0;
+    // unansweredQuestions = 0;
+   
 
-function run() {
-    clearInterval(intervalId);
-    intervalID = setInterval(decrement, 3000);
-}
+};
 
-function decrement() {
-    number--;
-    $("#timer").html("<h2>" + timeLeft + "</h2>");
+    $("#startBtn").click(function () {
+        startGame();
 
-    if(number === 0) {
-        stop();
-        alert("TIME IS UP!!!")
-        function stop() {
-            clearInterval(intervalId)
-        }
-        run();
+        $("#timer").text("00:00");
+
+        var gameTime = setInterval(function () {
+            console.log("game")
+            decrement();
+            if (timeLeft === 0) {
+                stop();
+                alert("TIME IS UP")
+                function stop() {
+                    clearInterval(setInterval)
+                    document.getElementById("timer").innerHTML = "TIME IS UP!!";
+                    run();
+                }
+                
+            };
+        }, 1000)
         
-    }
-}
+        console.log(timeLeft)
+
+        function run() {
+            clearInterval(intervalId);
+            intervalID = setInterval(decrement, 1000);
+            
+        };
+
+        function decrement() {
+            timeLeft--;
+            $("#timer").html("<h2>" + timeLeft + "</h2>");
+        };
+
+
+        
+        //   when timer reaches 0 alert quiz taker there is no more time. also show the questions that were right and the questions that were wrong.
+        // if (distance < 0) {
+        //     clearInterval(x);
+        //     document.getElementById(".container").innerHTML = "Times Up!!!";
+        // };
+
+    });
 
 
 
 
-$("timer").text("00:00");
-
-
-// update counter (count down) every 1 second
 
 
 
 
-// calculate for minuets and seconds
 
-// var deadline = new timer("0:03:00").getTime();
 
-// show the results as it counts down
 
-// document.getElementsById("count").innerHTML = minutes + "m" + seconds + "s";
 
-// when timer reaches 0 alert quiz taker there is no more time. also show the questions that were right and the questions that were wrong.
-// if( distance < 0) {
-//     clearInterval (x);
-//     document.getElementById("count").innerHTML = "Times Up!!!";
-// };
 
-// show a restart button
+    // var deadline = new timer("0:03:00").getTime();
 
+    // show the results as it counts down
+
+    // document.getElementsById("count").innerHTML = minutes + "m" + seconds + "s"
